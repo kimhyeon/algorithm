42eb92962c9f02ccc7148481857b31f596637b2a
kimhyeon <dudu753951@naver.com>
2018-08-27 17:09:47 +0900

    123

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int[] data = {2, 4, 6, 8, 10, 12, 14};
+        int[] data = {2, 4, 6, 8, 10, 12, 16};
         int target = 2;
     
         int result = BinarySearch2(data, target);
