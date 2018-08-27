8d1ed0c5c3d9f5068d1c9a911b907d02e7402fc5
kimhyeon <dudu753951@naver.com>
2018-08-27 16:37:05 +0900

    init!

---
+++ search/BinarySearch.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] data = {2, 4, 6, 8, 10, 12};
+        int target = 3;
+    
+        int result = BinarySearch2(data, target);
+        System.out.println("* " + result);
+    }
+
+    public static int BinarySearch2(int arr[], int target) {
+        int begin = 0;
+        int end = arr.length - 1;
+        int mid = arr.length / 2;
+
+        int result = arr[mid];
+
+        while(begin <= end) {  
+            if(result == target) {
+                return mid;
+            } else if(result < target) {
+                begin = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            mid = (begin + end) / 2;     
+            result = arr[mid];
+        }
+
+        return -1;
+    
+    }
+
+}
